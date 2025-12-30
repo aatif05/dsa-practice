30ce218042d1ea64b67486b490a11338ebbc9dba
aatif05 <mohammedaatifrizwan@gmail.com>
2025-12-30 19:35:18 +0530

    Binary search

--- problems/searching/BinarySeach.java
+++ problems/searching/BinarySeach.java
@@ -1,8 +1,8 @@
 public class BinarySeach {
     public static void main(String[] args) {
         int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-        int target = 7;
-        int result = binarySearch(arr, target);
+        int target = 0;
+        boolean result = binarySearch(arr, target);
         System.out.println(result);
     }
 
@@ -12,8 +12,15 @@ public class BinarySeach {
         while (left <= right) {
             int mid = left + (right - left) / 2;
             if (arr[mid] == target) {
-                return mid;
+                return true;
+            }
+            else if (arr[mid] < target) {
+                left = mid + 1;
+            }
+            else {
+                right = mid - 1;
             }
         }
+        return false;
     }
 }
