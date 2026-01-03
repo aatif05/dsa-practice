cd3b19a79373a477f04566a4225a685d05d308d0
aatif05 <mohammedaatifrizwan@gmail.com>
2026-01-03 16:49:47 +0530

    with example foe all number using prefix sum

--- problems/slidingwindow-twopointer/BinarySubArrayWithSum.java
+++ problems/slidingwindow-twopointer/BinarySubArrayWithSum.java
@@ -30,3 +30,15 @@ public class BinarySubArrayWithSum {
         return count;
     }
 }
+/*
+This prefix sum works with every number in the array.
+Map<Integer, Integer> prefix = new HashMap<>();
+prefix.put(0, 1);
+int sum = 0, count = 0;
+for (int num : nums) {
+    sum += num;
+    count += prefix.getOrDefault(sum - goal, 0);
+    prefix.put(sum, prefix.getOrDefault(sum, 0) + 1);
+}
+return count;
+ */
